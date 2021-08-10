dcf5f5c6d7eddc57dd8276dc2451371bc74f1def
Joshua Hoblitt <josh@hoblitt.com>
2021-08-10 14:45:47 -0700

    add has_dellperc fact

---
+++ lib/facter/has_dellperc.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Fact:
+#   has_dellperc
+#
+# Purpose:
+#   Return true if a Dell PERC (LSI MegaRAID) controller is installed
+#
+# Resolution:
+#   On physical hosts with the lspci binary, parse the output for known MegaRAID
+#   strings.
+#
+Facter.add('has_dellperc') do
+  confine kernel: :linux
+  confine is_virtual: false
+
+  setcode do
+    haslspci = Facter::Util::Resolution.exec('which lspci')
+    if haslspci.nil?
+      # no lspci on this system
+    else
+      output = Facter::Util::Resolution.exec("#{haslspci} -v")
+      if output.match(%r{Dell PERC}i)
+        true
+      else
+        false
+      end
+    end
+  end
+end
---
+++ spec/facts/has_dellperc_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe 'has_dellperc', :type => :fact do
+  before(:each) { Facter.clear }
+  subject(:fact) { Facter.fact(:has_dellperc) }
+
+  context 'on linux' do
+    context 'on physical hardware' do
+      context 'lspci not in path' do
+        it do
+          allow(Facter.fact(:kernel)).to receive(:value) { 'Linux' }
+          allow(Facter.fact(:is_virtual)).to receive(:value) { false }
+          allow(Facter::Util::Resolution).to receive(:exec).with('which lspci') { nil }
+
+          expect(subject.value).to be_nil
+        end
+      end
+
+      context 'lspci output does not match' do
+        it do
+          allow(Facter.fact(:kernel)).to receive(:value) { 'Linux' }
+          allow(Facter.fact(:is_virtual)).to receive(:value) { false }
+          allow(Facter::Util::Resolution).to receive(:exec).with('which lspci') do
+            '/usr/sbin/lspci'
+          end
+          allow(Facter::Util::Resolution).to receive(:exec).with('/usr/sbin/lspci -v') do
+            File.read(fixtures('facts', 'lspci_no_match'))
+          end
+
+          expect(subject.value).to eq false
+        end
+      end
+
+      context 'lspci output does match' do
+        it do
+          allow(Facter.fact(:kernel)).to receive(:value) { 'Linux' }
+          allow(Facter.fact(:is_virtual)).to receive(:value) { false }
+          allow(Facter::Util::Resolution).to receive(:exec).with('which lspci') do
+            '/usr/sbin/lspci'
+          end
+          allow(Facter::Util::Resolution).to receive(:exec).with('/usr/sbin/lspci -v') do
+            File.read(fixtures('facts', 'lspci_match'))
+          end
+
+          expect(subject.value).to eq true
+        end
+      end
+    end
+
+    context 'on virtual hardware' do
+      it do
+        allow(Facter.fact(:kernel)).to receive(:value) { 'Linux' }
+        allow(Facter.fact(:is_virtual)).to receive(:value) { true }
+
+        expect(subject.value).to be_nil
+      end
+    end
+
+  end # on linux
+
+  context 'not on linux' do
+    it do
+      allow(Facter.fact(:kernel)).to receive(:value) { 'Solaris' }
+      allow(Facter.fact(:is_virtual)).to receive(:value) { false }
+
+      expect(subject.value).to be_nil
+    end
+  end # not on linux
+end
