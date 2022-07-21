61fa9e9f769c957d6b79072f9525b82cbaf9271e
Joshua Hoblitt <josh@hoblitt.com>
2022-07-21 09:15:56 -0700

    add spec coverage for all metadata.json supported OS

--- spec/unit/has_dellperc_spec.rb
+++ spec/unit/has_dellperc_spec.rb
@@ -7,54 +7,58 @@ describe 'has_dellperc', type: :fact do
 
   before(:each) { Facter.clear }
 
-  context 'when on linux' do
-    context 'when on physical hardware' do
-      context 'when lspci not in path' do
+  on_supported_os.each do |os, facts|
+    let(:facts) { facts }
+
+    context "on #{os}" do
+      context 'when on physical hardware' do
+        context 'when lspci not in path' do
+          it do
+            allow(Facter.fact(:kernel)).to receive(:value).and_return('Linux')
+            allow(Facter.fact(:is_virtual)).to receive(:value).and_return(false)
+            allow(Facter::Util::Resolution).to receive(:exec).with('which lspci').and_return(nil)
+
+            expect(fact.value).to be_nil
+          end
+        end
+
+        context 'when lspci output does not match' do
+          it do
+            allow(Facter.fact(:kernel)).to receive(:value).and_return('Linux')
+            allow(Facter.fact(:is_virtual)).to receive(:value).and_return(false)
+            allow(Facter::Util::Resolution).to receive(:exec).with('which lspci').and_return('/usr/sbin/lspci')
+            allow(Facter::Util::Resolution).to receive(:exec).with('/usr/sbin/lspci -v') do
+              File.read(fixtures('facts', 'lspci_no_match'))
+            end
+
+            expect(fact.value).to be false
+          end
+        end
+
+        context 'when lspci output does match' do
+          it do
+            allow(Facter.fact(:kernel)).to receive(:value).and_return('Linux')
+            allow(Facter.fact(:is_virtual)).to receive(:value).and_return(false)
+            allow(Facter::Util::Resolution).to receive(:exec).with('which lspci').and_return('/usr/sbin/lspci')
+            allow(Facter::Util::Resolution).to receive(:exec).with('/usr/sbin/lspci -v') do
+              File.read(fixtures('facts', 'lspci_match'))
+            end
+
+            expect(fact.value).to be true
+          end
+        end
+      end
+
+      context 'when on virtual hardware' do
         it do
           allow(Facter.fact(:kernel)).to receive(:value).and_return('Linux')
-          allow(Facter.fact(:is_virtual)).to receive(:value).and_return(false)
-          allow(Facter::Util::Resolution).to receive(:exec).with('which lspci').and_return(nil)
+          allow(Facter.fact(:is_virtual)).to receive(:value).and_return(true)
 
           expect(fact.value).to be_nil
         end
       end
-
-      context 'when lspci output does not match' do
-        it do
-          allow(Facter.fact(:kernel)).to receive(:value).and_return('Linux')
-          allow(Facter.fact(:is_virtual)).to receive(:value).and_return(false)
-          allow(Facter::Util::Resolution).to receive(:exec).with('which lspci').and_return('/usr/sbin/lspci')
-          allow(Facter::Util::Resolution).to receive(:exec).with('/usr/sbin/lspci -v') do
-            File.read(fixtures('facts', 'lspci_no_match'))
-          end
-
-          expect(fact.value).to be false
-        end
-      end
-
-      context 'when lspci output does match' do
-        it do
-          allow(Facter.fact(:kernel)).to receive(:value).and_return('Linux')
-          allow(Facter.fact(:is_virtual)).to receive(:value).and_return(false)
-          allow(Facter::Util::Resolution).to receive(:exec).with('which lspci').and_return('/usr/sbin/lspci')
-          allow(Facter::Util::Resolution).to receive(:exec).with('/usr/sbin/lspci -v') do
-            File.read(fixtures('facts', 'lspci_match'))
-          end
-
-          expect(fact.value).to be true
-        end
-      end
-    end
-
-    context 'when on virtual hardware' do
-      it do
-        allow(Facter.fact(:kernel)).to receive(:value).and_return('Linux')
-        allow(Facter.fact(:is_virtual)).to receive(:value).and_return(true)
-
-        expect(fact.value).to be_nil
-      end
-    end
-  end # on linux
+    end # context "on #{os}"
+  end # on_supported_os
 
   context 'when not on linux' do
     it do
